765b2197a1c1d0e278fab7e199f00f9f97fc7738
Narabodee Rodjananant <martnarabodee@gmail.com>
2021-11-22 16:40:05 +0700

    init

--- oml/oml/oml/settings.py
+++ oml/oml/oml/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0)v3)mzkra+s59^fse(g=3^aq+l8xt9gq8!bfthz*+@v&uu%fj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
